1c9c45973360368200cb8d90a0bdc180d6182a2e
God4Ever <erosenan@gmail.com>
2020-05-15 16:52:31 +0300

    Backup

--- Calculator/src/gali/Main.java
+++ Calculator/src/gali/Main.java
@@ -1,33 +1,58 @@
 package gali;
 
+import java.io.IOException;
+import java.io.StreamTokenizer;
+import java.io.StringReader;
 import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter an equetion");
 		String eq = scan.next();
 		Expr expr = Expr.parse(eq);
 		System.out.println(expr);
 		System.out.println(expr.eval());
+		
+//		String s = "if(a == 2) { print(3); }";
+//		StreamTokenizer tokenizer = new StreamTokenizer(new StringReader(s));
+//		while(tokenizer.ttype != StreamTokenizer.TT_EOF) {
+//			tokenizer.nextToken();
+//			switch(tokenizer.ttype) {
+//			case StreamTokenizer.TT_WORD:
+//				System.out.println("Word: " + tokenizer.sval);
+//				break;
+//			case StreamTokenizer.TT_NUMBER:
+//				System.out.println("Number: " + tokenizer.nval);
+//				break;
+//			case StreamTokenizer.TT_EOF:
+//				break;
+//			default:
+//				System.out.println("Symbol: " + (char)tokenizer.ttype);				
+//			}
+//		}
+		double a = 0;
+		String s = scan.next();
+		StreamTokenizer tokenizer = new StreamTokenizer(new StringReader(s));
+		tokenizer.ordinaryChar('-');
+		while(tokenizer.ttype != StreamTokenizer.TT_EOF) {
+			tokenizer.nextToken();
+			switch(tokenizer.ttype) {
+			case StreamTokenizer.TT_NUMBER:
+				a = a + (double)tokenizer.nval;//System.out.println("Number: " + tokenizer.nval);
+				break;
+			case StreamTokenizer.TT_EOF:
+				break;
+			default:
+				System.out.println("Symbol: " + (char)tokenizer.ttype);				
+			}
+		}
+		System.out.println(a);	
+		//gali(tokenizer);
 	}
-	/*public static int calc(String expr) {
-		int opPos = Math.max(expr.lastIndexOf("+"), expr.lastIndexOf("-"));
-
-		if (opPos == -1){
-			return new Integer(expr);
-		}
-		else {
-			String right = expr.substring(opPos +1);
-			String left = expr.substring(0, opPos);
-			if (expr.charAt(opPos) == '+') {
-				return calc(left) + calc(right);
-			}
-			else {
-				return calc(left) - calc(right);
-			}
-		}
-	}*/
-
+	private static void gali(StreamTokenizer tokenizer) {
+		double a = tokenizer.nval;
+		System.out.println(a);
+	}
 }
